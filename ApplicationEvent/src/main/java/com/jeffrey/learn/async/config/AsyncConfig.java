94bc9914dbc3aad3dc417fcce922f6e3180f56dd
刘春生 <906956368@qq.com>
2020-01-11 15:15:20 +0800

    feat: 自定义线程池

---
+++ ApplicationEvent/src/main/java/com/jeffrey/learn/async/config/AsyncConfig.java
@@ -0,0 +1,74 @@
+package com.jeffrey.learn.async.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author: jeffrey
+ * @date: 1/11/20 2:28 PM
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    private static final Logger logger = LoggerFactory.getLogger(AsyncConfig.class);
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        // 核心线程数
+        taskExecutor.setCorePoolSize(5);
+        // 最大线程数
+        taskExecutor.setMaxPoolSize(20);
+        // 队列大小
+        taskExecutor.setQueueCapacity(1000);
+        // 线程最大空闲时间
+        taskExecutor.setKeepAliveSeconds(300);
+        // 线程名的前缀
+        taskExecutor.setThreadNamePrefix("async-thread-");
+        // 拒绝策略
+        taskExecutor.setRejectedExecutionHandler(
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+        /**
+         * 拒绝策略有四种
+         * ThreadPoolExecutor.AbortPolicy 丢弃任务并抛出RejectedExecutionException异常(默认)。
+         * ThreadPoolExecutor.DiscardPolic 丢弃任务，但是不抛出异常。
+         * ThreadPoolExecutor.DiscardOldestPolicy 丢弃队列最前面的任务，然后重新尝试执行任务
+         * ThreadPoolExecutor.CallerRunsPolic 由调用线程处理该任务
+         */
+
+
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    /**
+     * 捕捉IllegalArgumentException异常
+     */
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new MyAsyncExceptionHandler();
+    }
+
+    static class MyAsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+        @Override
+        public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+            logger.error("Task Exception message: {}", ex.getMessage());
+            logger.error("Method Name: {}", method.getName());
+            for (Object param : params) {
+                logger.error("Parameter value: {}", param);
+            }
+        }
+    }
+}
