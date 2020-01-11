c4f6a95c0fdbb85e9aa9dca982295dc8ec0c2363
刘春生 <906956368@qq.com>
2020-01-11 15:15:43 +0800

    feat: 使用SpringBoot线程池

--- ApplicationEvent/src/main/java/com/jeffrey/learn/LearnApplication.java
+++ ApplicationEvent/src/main/java/com/jeffrey/learn/LearnApplication.java
@@ -2,7 +2,10 @@ package com.jeffrey.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+// 这里配置EnableAsync则使用springBoot默认线程池
+//@EnableAsync
 @SpringBootApplication
 public class LearnApplication {
 
